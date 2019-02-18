93e602f04ae6e5f0377bc414de0350608fc0d963
Hunter Freyer <yt@hjfreyer.com>
2019-02-18 14:28:39 -0500

    Kinda got light rendering working.

--- render.py
+++ render.py
@@ -32,17 +32,28 @@ def invert(dist):
 def pt(x, y, z):
     return np.array([x, y, z], dtype=np.float32)
 
+def direction(x, y, z):
+    return tf.math.l2_normalize(tf.constant([x, y, z], dtype=np.float32))
+
 
 #camera = np.array([0, 0, 0], dtype=np.float32)
+WIDTH = 500
+HEIGHT = 500
+
+camera_src = pt(0, 0, 0)
+camera_point = direction(1, 0, 0)
+camera_tilt = direction(0, 0, 1)
 focal_length = 1.0
-pixels2d = np.stack(np.mgrid[-1:1:0.001, -1:1:0.001])
+
+pixels2d = np.stack(np.mgrid[-1:1:(2.0/WIDTH), -1:1:(2.0/HEIGHT)])
 pixels3d = np.concatenate([np.full((1,)+ pixels2d.shape[1:], focal_length), pixels2d])
-rays = pixels3d
+rays = np.reshape(pixels3d, (3, WIDTH*HEIGHT))
 #
 # rays = np.reshape(pixels3d, (3, -1))
 # rays = np.swapaxes(rays, 0, 1)
 
 Surface = collections.namedtuple('Surface', 'distance color')
+Emitter = collections.namedtuple('Emitter', 'source color')
 
 surfaces = [
     Surface(distance=sphere_distance(pt(5, 0, 0), 1),
@@ -51,43 +62,59 @@ surfaces = [
             color=pt(0, 0, 1)),
 ]
 surface_colors = np.stack([s.color for s in surfaces])
+emitters = [
+    Emitter(source=pt(2,2,2), color=pt(1,1,1))
+]
 
-Surface = collections.namedtuple('Surface', 'distance')
+emitter = emitters[0]
 
 def march_op(prop, dist):
     return tf.group(prop.assign_add(dist))
 
 with tf.Session() as sess:
-    prop = tf.Variable(np.zeros(rays.shape[1:]), dtype=np.float32)
+    prop = tf.Variable(np.zeros(rays.shape[1]), dtype=np.float32)
 
     ray_ends = rays * tf.expand_dims(prop, 0)
     surface_dists = tf.stack([s.distance(ray_ends) for s in surfaces])
-    converged = surface_dists < 3
-    point_converged = tf.math.reduce_any(converged, axis=0)
-    surface_min_dist = tf.math.reduce_min(surface_dists, axis=0)
 
-    #opt = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-#    opt_op = opt.minimize(surface_dist**2, var_list=[prop])
-    opt_op = march_op(prop, surface_min_dist)
+    closest_surface_arg = tf.argmin(surface_dists, axis=0)
+    closest_surface_dist = tf.math.reduce_min(surface_dists, axis=0)
+    all_converged = tf.math.reduce_all(closest_surface_dist < 0.01)
+
+    opt_op = march_op(prop, closest_surface_dist)
+
+    normals, = tf.gradients(closest_surface_dist, ray_ends)
+    normals = tf.math.l2_normalize(normals, axis=0)
+
+    emitter_to_pts = ray_ends - tf.expand_dims(emitter.source, 1)
+    emitter_dot_products = tf.reduce_sum(
+        tf.math.l2_normalize(emitter_to_pts, axis=0)*normals, axis=0)
+
+
 
     tf.global_variables_initializer().run()
-        #PROP, RES = sess.run([point_converged, surface_min_dist])
-        #RES = np.minimum(RES, 1.0)
-        #plt.imshow(RES)
-        #plt.imshow(PROP)
-        #plt.show()
-    all_converged = tf.math.reduce_all(point_converged)
-    ball_converged = False
+
+    print('Graph built')
+
     idx = 0
-    while not ball_converged:
-        _, ball_converged = sess.run([opt_op, all_converged])
+    while True:
         print(idx)
-        idx+=1
-    touched_surface = tf.argmin(surface_dists, axis=0)
-    color = tf.gather(surface_colors, touched_surface)
+        _, r_all_converged, r_closest_surface_arg = sess.run(
+            [opt_op, all_converged, closest_surface_arg])
+        idx += 1
+        if r_all_converged:
+            break
+
+    color = tf.stack([emitter_dot_products] * 3)
+    color = tf.reshape(color, (3, WIDTH, HEIGHT))
+    color = tf.transpose(color)
+    # touched_surface = tf.argmin(surface_dists, axis=0)
+    # color = tf.gather(surface_colors, touched_surface
     c = sess.run(color)
     plt.imshow(c)
     plt.show()
+
+    print('Get normals')
 
 
 
