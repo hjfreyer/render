54f7ed1e6ecf864e471d1527a27ee3ad1615afb5
Hunter Freyer <yt@hjfreyer.com>
2019-02-25 21:06:50 -0500

    Boom! Shadows!

--- render.py
+++ render.py
@@ -38,17 +38,26 @@ ray_dirs = tf.math.l2_normalize(pixels3d, axis=1)
 ray_coords = tf.zeros_like(
     ray_dirs) + tf.expand_dims(tf.constant([-3, 0, 1], dtype=tf.float64), axis=0)
 
-rays = tf.concat([ray_coords, ray_dirs], axis=1)
 
+Ray = collections.namedtuple('Ray', 'root direction')
 Surface = collections.namedtuple('Surface', 'geometry color')
 Emitter = collections.namedtuple('Emitter', 'source color')
 Scene = collections.namedtuple('Scene', 'surfaces emitters')
+
+rays = Ray(root=ray_coords, direction=ray_dirs)
+
+
+def ray_gather_nd(ray, *args, **kwargs):
+    root = tf.gather_nd(ray.root, *args, **kwargs)
+    dir = tf.gather_nd(ray.direction, *args, **kwargs)
+    return Ray(root, dir)
+
 
 SCENE = Scene(
     surfaces=[
         Surface(geometry=geom.Sphere(pt(5, 0, 0), 1),
                 color=pt(1, 0, 1)),
-        Surface(geometry=geom.Sphere(pt(2.5, 2.5, 2.5), 0.5),
+        Surface(geometry=geom.Sphere(pt(2.5, 2.5, 2.5), 0.1),
                 color=pt(1, 1, 0)),
         Surface(geometry=geom.Intersection([geom.Plane(pt(10, 0, 0), direction(-1, -1, 0)),
                                             geom.Plane(pt(10, 0, 0), direction(0, 0, 1))]),
@@ -85,7 +94,7 @@ def gamma_correct(colors):
 
 
 def get_closest_surface_distance(rays, lengths):
-    ray_ends = rays[:, 0:3] + rays[:, 3:6] * tf.expand_dims(lengths, 1)
+    ray_ends = rays.root + rays.direction * tf.expand_dims(lengths, 1)
     surface_dists = tf.stack([s.geometry.distance(ray_ends)
                               for s in SCENE.surfaces], axis=1)
     return tf.math.reduce_min(surface_dists, axis=1), tf.argmin(surface_dists, axis=1)
@@ -97,11 +106,11 @@ def propagate_rays(surfaces, rays):
 
     def body(converged, lengths):
         unconverged_idxes = tf.where(tf.math.logical_not(converged))
-        unconverged_rays = tf.gather_nd(rays, unconverged_idxes)
+        unconverged_rays = ray_gather_nd(rays, unconverged_idxes)
         unconverged_lengths = tf.gather_nd(lengths, unconverged_idxes)
 
-        ray_ends = (unconverged_rays[:, 0:3]
-                    + unconverged_rays[:, 3:6] * tf.expand_dims(unconverged_lengths, 1))
+        ray_ends = unconverged_rays.root + unconverged_rays.direction * \
+            tf.expand_dims(unconverged_lengths, 1)
 
         surface_dists = tf.stack([s.geometry.distance(ray_ends)
                                   for s in SCENE.surfaces], axis=1)
@@ -110,22 +119,79 @@ def propagate_rays(surfaces, rays):
         converged = tf.math.logical_or(
             converged,
             tf.scatter_nd(unconverged_idxes, min_surface_dists < CONVERGED, converged.shape))
+
         lengths += tf.scatter_nd(unconverged_idxes,
                                  min_surface_dists, lengths.shape)
 
         return converged, lengths
 
     _, lengths = tf.while_loop(cond, body, (
-        tf.zeros(rays.shape[0], dtype=tf.bool),
-        tf.zeros(rays.shape[0], dtype=tf.float64),
+        tf.zeros(rays.root.shape[:-1], dtype=tf.bool),
+        tf.zeros(rays.root.shape[:-1], dtype=tf.float64),
     ))
     return lengths
 
 
+def get_clearance(surfaces, points):
+    def cond(too_close, points):
+        return tf.math.reduce_any(too_close)
+
+    def body(too_close, points):
+        too_close_idxes = tf.where(too_close)
+        too_close_points = tf.gather_nd(points, too_close_idxes)
+
+        surface_dists = tf.stack([s.geometry.distance(too_close_points)
+                                  for s in SCENE.surfaces], axis=1)
+        min_surface_dists = tf.math.reduce_min(surface_dists, axis=1)
+
+        gradients, = tf.gradients(min_surface_dists, too_close_points)
+
+        # This isn't quite right. Also not quite right in propagate. Each goes one loop too far.
+        points_delta = gradients * CONVERGED
+        new_too_close = min_surface_dists < CONVERGED
+
+        points += tf.scatter_nd(too_close_idxes, points_delta, points.shape)
+        too_close = tf.math.logical_and(
+            too_close,
+            tf.scatter_nd(too_close_idxes, new_too_close, too_close.shape))
+
+        return too_close, points
+    _, points = tf.while_loop(cond, body, (
+        tf.fill(points.shape[:1], True),
+        points
+    ))
+    return points
+
+
 with tf.Session() as sess:
+    emitters_sources = tf.stack([e.source for e in SCENE.emitters], axis=0)
+    emitters_colors = tf.stack([e.color for e in SCENE.emitters], axis=0)
+
+    rays = rays._replace(root=get_clearance(SCENE.surfaces, rays.root))
     lengths = propagate_rays(SCENE.surfaces, rays)
 
-    ray_ends = rays[:, 0:3] + rays[:, 3:6] * tf.expand_dims(lengths, 1)
+    reflection_points = rays.root + rays.direction * tf.expand_dims(lengths, 1)
+    reflection_points = get_clearance(SCENE.surfaces, reflection_points)
+
+    reflection_points_to_emitters_vector = (tf.expand_dims(emitters_sources, axis=0)
+                                            - tf.expand_dims(reflection_points, axis=1))
+    reflection_points_to_emitters_dir = tf.math.l2_normalize(
+        reflection_points_to_emitters_vector, axis=2)
+
+    reflection_points = tf.broadcast_to(tf.expand_dims(
+        reflection_points, 1), tf.shape(reflection_points_to_emitters_dir))
+
+    reflected_rays = Ray(root=reflection_points,
+                         direction=reflection_points_to_emitters_dir)
+    reflected_lengths = propagate_rays(SCENE.surfaces, reflected_rays)
+    reflected_lengths = tf.reshape(
+        reflected_lengths, (-1, len(SCENE.emitters)))
+
+    distance_to_emitters = tf.norm(
+        reflection_points_to_emitters_vector, axis=2)
+    unobstructed = distance_to_emitters < reflected_lengths
+
+    ray_ends = rays.root + rays.direction * tf.expand_dims(lengths, 1)
     surface_dists = tf.stack([s.geometry.distance(ray_ends)
                               for s in SCENE.surfaces], axis=1)
     closest_surface_dist = tf.math.reduce_min(surface_dists, axis=1)
@@ -134,17 +200,11 @@ with tf.Session() as sess:
     normals, = tf.gradients(closest_surface_dist, ray_ends)
     normals = tf.math.l2_normalize(normals, axis=1)
 
-    emitters_sources = tf.stack([e.source for e in SCENE.emitters], axis=0)
-    emitters_colors = tf.stack([e.color for e in SCENE.emitters], axis=0)
-
-    pts_to_emitters = (tf.expand_dims(emitters_sources, axis=0)
-                       - tf.expand_dims(ray_ends, axis=1))
-    pts_to_emitters = tf.math.l2_normalize(pts_to_emitters, axis=2)
-
     emitters_dot_products = tf.reduce_sum(
-        pts_to_emitters * tf.expand_dims(normals, axis=1),
+        reflection_points_to_emitters_dir * tf.expand_dims(normals, axis=1),
         axis=2)
     emitters_dot_products = tf.maximum(emitters_dot_products, 0)
+    emitters_dot_products *= tf.cast(unobstructed, tf.float64)
 
     emitters_colors_per_point = tf.expand_dims(
         emitters_colors, axis=0) * tf.expand_dims(emitters_dot_products, axis=2)
